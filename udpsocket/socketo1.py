fb6fbb10eb556ea895be79708c8deae8b156db61
gongyangyu <1665465903@qq.com>
2019-08-27 22:27:49 +0800

    #version1# 用户输入数据

--- udpsocket/socketo1.py
+++ udpsocket/socketo1.py
@@ -6,7 +6,8 @@ def main():
     #发送数据
     ip="192.168.0.102"
     port=8080
-    udp_socket.sendto(b"hhhha",(ip,port))
+    send_data=input("请输入数据：")
+    udp_socket.sendto(send_data.encode("utf-8"),(ip,port))
     #关闭socket
     udp_socket.close();
 if __name__=="__main__":
