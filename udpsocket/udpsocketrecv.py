e362f871fc43254d9535d1d96cb9ceb64e8cee6e
gongyangyu <1665465903@qq.com>
2019-08-28 21:33:09 +0800

    #version1# 循环接收数据

--- udpsocket/udpsocketrecv.py
+++ udpsocket/udpsocketrecv.py
@@ -6,9 +6,13 @@ def main():
     #绑定地址
     locAddr=("",7788)
     socket_udp.bind(locAddr)
-    #接收数据
-    recv_data=socket_udp.recvfrom(1024)
-    print(recv_data)
+    while True:
+        #接收数据
+        recv_data=socket_udp.recvfrom(1024)
+        (recv_msg,remote_addr)=recv_data;
+        print("%s 发来的消息是:%s" % (str(remote_addr),recv_msg.decode("gbk")))
+        if recv_msg==b'over':
+            break
     #关闭套接字
     socket_udp.close()
 
