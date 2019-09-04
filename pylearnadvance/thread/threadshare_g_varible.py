947282b4d24a7064c0803a97860cd08e8e5fd9b3
gongyangyu <1665465903@qq.com>
2019-09-04 22:23:41 +0800

    #version1# 多线程共享全局变量资源竞争

--- pylearnadvance/thread/threadshare_g_varible.py
+++ pylearnadvance/thread/threadshare_g_varible.py
@@ -2,26 +2,32 @@ import threading
 import time
 
 
-def test1(temp):
-    temp.append(34)
-    print("-------g_num ---test1 %s" % str(temp))
+total=0
+
+def test1(number):
+    global total
+    for i in range(number):
+        total+=1
+    print("-------g_num ---test1 %s" % str(total))
 
 
-def test2(temp):
-    print("-------g_num ---test2 %s" % str(temp))
+def test2(number):
+    global total
+    for i in range(number):
+        total+=1
+    print("-------g_num ---test2 %s" % str(total))
 
 
-g_num=[23,24]
+g_num=1000000
 
 def main():
     t1=threading.Thread(target=test1,args=(g_num,))
     t2=threading.Thread(target=test2,args=(g_num,))
     t1.start()
-    time.sleep(1)
     t2.start()
-    time.sleep(1)
 
-    print("-------g_num ---main %s" % str(g_num))
+    time.sleep(5)
+    print("-------g_num ---main %s" % str(total))
 
 if __name__ == "__main__":
     main()
