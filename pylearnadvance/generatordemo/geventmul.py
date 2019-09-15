6c3c3868ec3519ee8bb975374e3adc63a7fe32d6
gongyangyu <1665465903@qq.com>
2019-09-15 18:44:06 +0800

    #version1# gevent 实现多任务

--- pylearnadvance/generatordemo/geventmul.py
+++ pylearnadvance/generatordemo/geventmul.py
@@ -1,25 +1,25 @@
+import time
+
 import gevent
+from gevent import monkey
+
+monkey.patch_all()
 
 def f1(n):
     for i in range(n):
         print(gevent.getcurrent(),i)
-        gevent.sleep(0.5)
-
+        time.sleep(0.5)
 
 def f2(n):
     for i in range(n):
         print(gevent.getcurrent(),i)
-        gevent.sleep(0.5)
+        time.sleep(0.5)
 
 
 def f3(n):
     for i in range(n):
         print(gevent.getcurrent(),i)
-        gevent.sleep(0.5)
+        time.sleep(0.5)
 
-g1=gevent.spawn(f1,5)
-g2=gevent.spawn(f2,7)
-g3=gevent.spawn(f3,10)
-g1.join()
-g2.join()
-g3.join()
+gevent.joinall([gevent.spawn(f1,10),gevent.spawn(f2,10),gevent.spawn(f3,10)])
+
